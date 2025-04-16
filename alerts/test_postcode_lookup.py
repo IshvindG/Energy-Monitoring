be6337b68a1f1c9dccb512a0b47d81fa15f83c1e
Hadiaf99 <hadiaf@hotmail.com>
2025-04-16 16:14:47 +0100

    adding tests

---
+++ alerts/test_postcode_lookup.py
@@ -0,0 +1,50 @@
+from postcode_lookup import get_region_from_postcode, find_provider_from_region, get_connection_to_db, find_provider_from_region_id
+import pytest
+from dotenv import load_dotenv
+import os
+
+
+def test_get_region_from_postcode():
+    assert get_region_from_postcode('E1 7DB') == 'London'
+    assert get_region_from_postcode('FK2 8FX') == 'Scotland'
+
+
+def test_get_region_from_postcode_error():
+
+    with pytest.raises(ValueError):
+        get_region_from_postcode('12fsasd')
+        get_region_from_postcode('sdfji0348')
+
+
+def test_region_mappings_works():
+
+    assert get_region_from_postcode("LL57 2DG") == "Wales"
+
+
+def test_get_provider_from_region():
+    db_connection = get_connection_to_db()
+    db_cursor = db_connection.cursor()
+    assert find_provider_from_region(db_cursor, 'London') == 6
+    assert find_provider_from_region(db_cursor, 'Wales') == 2
+
+
+def test_get_provider_from_region_fake_region():
+    db_connection = get_connection_to_db()
+    db_cursor = db_connection.cursor()
+    with pytest.raises(ValueError):
+        find_provider_from_region(db_cursor, 'Cairo')
+        find_provider_from_region(db_cursor, 'fake')
+
+
+def test_get_provider_from_region_id():
+    db_connection = get_connection_to_db()
+    db_cursor = db_connection.cursor()
+    assert find_provider_from_region_id(db_cursor, 30) == (6, 'London')
+    assert find_provider_from_region_id(db_cursor, 34) == (2, 'Wales')
+
+
+def test_get_provider_from_region_id_fake_region_id():
+    db_connection = get_connection_to_db()
+    db_cursor = db_connection.cursor()
+    with pytest.raises(ValueError):
+        find_provider_from_region_id(db_cursor, 123)
